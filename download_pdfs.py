1337a1702237db6a447c2ecbcd8b4acb34a33fd2
Aleksandr Gomelskii Kramar <6369029@gmail.com>
2025-02-10 20:34:33 +0200

    Module for daownloading pdfs for dataset prparation

---
+++ download_pdfs.py
@@ -0,0 +1,15 @@
+import urllib.request
+
+for i in range(1, 5407):
+
+    url = f'https://daf-yomi.com/Data/UploadedFiles/DY_Page/{i}.pdf'
+
+    req = urllib.request.Request(url, headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    })
+    data = None
+    with urllib.request.urlopen(req) as res:
+        data = res.read()
+    with open(f'pdfs/{i}.pdf', 'bw+') as file:
+        file.write(data)
+
