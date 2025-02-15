67ee6fc22c42564bc80f5007753f566a3709f8dd
Aleksandr Gomelskii Kramar <6369029@gmail.com>
2025-02-16 00:05:55 +0200

    fine tunnig of algo

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -7,247 +7,23 @@ import os
 from tqdm import tqdm 
 import random
 
-
 PDF_DIR = 'pdfs'
 INPUTS_DIR = 'inputs'
 OUTPUTS_DIR = 'outputs'
 
-ink_phase = [
-    Dithering(
-        dither=random.choice(["ordered", "floyd-steinberg"]),
-        order=(3, 5),
-        p=0.33,
-    ),
-    InkBleed(
-        intensity_range=(0.5, 0.6),
-        kernel_size=random.choice([(5, 5), (3, 3)]),
-        severity=(0.3, 0.4),
-        p=0.33,
-    ),
-    BleedThrough(
-        intensity_range=(0.1, 0.3),
-        color_range=(32, 224),
-        ksize=(17, 17),
-        sigmaX=1,
-        alpha=random.uniform(0.1, 0.2),
-        offsets=(10, 20),
-        p=0.33,
-    ),
-    Letterpress(
-        n_samples=(100, 400),
-        n_clusters=(200, 400),
-        std_range=(500, 3000),
-        value_range=(150, 224),
-        value_threshold_range=(96, 128),
-        blur=1,
-        p=0.33,
-    ),
-    OneOf(
-        [
-            LowInkRandomLines(
-                count_range=(5, 10),
-                use_consistent_lines=random.choice([True, False]),
-                noise_probability=0.1,
-            ),
-            LowInkPeriodicLines(
-                count_range=(2, 5),
-                period_range=(16, 32),
-                use_consistent_lines=random.choice([True, False]),
-                noise_probability=0.1,
-            ),
-        ],
-    ),
-]
+paperfactory = PaperFactory(texture_path="",
+                            generate_texture=1,
+                            generate_texture_background_type="random",
+                            generate_texture_edge_type="random",
+                            texture_enable_color=0,
+                            blend_texture=1,
+                            blend_generate_texture = 0,
+                            blend_texture_path='',
+                            blend_texture_background_type="random",
+                            blend_texture_edge_type="random",
+                            blend_method="ink_to_paper")
 
-paper_phase = [
-    PaperFactory(p=0.33),
-    ColorPaper(
-        hue_range=(0, 255),
-        saturation_range=(10, 40),
-        p=0.33,
-    ),
-    WaterMark(
-        watermark_word="random",
-        watermark_font_size=(10, 15),
-        watermark_font_thickness=(20, 25),
-        watermark_rotation=(0, 360),
-        watermark_location="random",
-        watermark_color="random",
-        watermark_method="darken",
-        p=0.33,
-    ),
-    OneOf(
-        [
-            AugmentationSequence(
-                [
-                    NoiseTexturize(
-                        sigma_range=(3, 10),
-                        turbulence_range=(2, 5),
-                    ),
-                    BrightnessTexturize(
-                        texturize_range=(0.9, 0.99),
-                        deviation=0.03,
-                    ),
-                ],
-            ),
-            AugmentationSequence(
-                [
-                    BrightnessTexturize(
-                        texturize_range=(0.9, 0.99),
-                        deviation=0.03,
-                    ),
-                    NoiseTexturize(
-                        sigma_range=(3, 10),
-                        turbulence_range=(2, 5),
-                    ),
-                ],
-            ),
-        ],
-        p=0.33,
-    ),
-    Brightness(
-        brightness_range=(0.9, 1.1),
-        min_brightness=0,
-        min_brightness_value=(120, 150),
-        p=0.1,
-    ),
-]
-
-post_phase = [
-    OneOf(
-        [
-            PageBorder(
-                page_border_width_height="random",
-                page_border_color=(0, 0, 0),
-                page_border_background_color=(0, 0, 0),
-                page_numbers="random",
-                page_rotation_angle_range=(0, 0),
-                curve_frequency=(2, 8),
-                curve_height=(2, 4),
-                curve_length_one_side=(50, 100),
-                same_page_border=random.choice([0, 1]),
-            ),
-            DirtyRollers(
-                line_width_range=(2, 32),
-                scanline_type=0,
-            ),
-        ],
-        p=0.33,
-    ),
-    OneOf(
-        [
-            LightingGradient(
-                light_position=None,
-                direction=None,
-                max_brightness=255,
-                min_brightness=0,
-                mode="gaussian",
-                linear_decay_rate=None,
-                transparency=None,
-            ),
-            Brightness(
-                brightness_range=(0.9, 1.1),
-                min_brightness=0,
-                min_brightness_value=(120, 150),
-            ),
-        ],
-        p=0.33,
-    ),
-    DirtyDrum(
-        line_width_range=(1, 6),
-        line_concentration=random.uniform(0.05, 0.15),
-        direction=random.randint(0, 2),
-        noise_intensity=random.uniform(0.6, 0.95),
-        noise_value=(64, 224),
-        ksize=random.choice([(3, 3), (5, 5), (7, 7)]),
-        sigmaX=0,
-        p=0.33,
-    ),
-    SubtleNoise(
-        subtle_range=random.randint(5, 10),
-        p=0.33,
-    ),
-    Jpeg(
-        quality_range=(25, 95),
-        p=0.33,
-    ),
-    Folding(
-        fold_x=None,
-        fold_deviation=(0, 0),
-        fold_count=random.randint(1, 6),
-        fold_noise=random.uniform(0, 0.2),
-        gradient_width=(0.1, 0.2),
-        gradient_height=(0.01, 0.02),
-        p=0.33,
-    ),
-    Markup(
-        num_lines_range=(2, 7),
-        markup_length_range=(0.5, 1),
-        markup_thickness_range=(1, 2),
-        markup_type=random.choice(["strikethrough", "crossed", "highlight", "underline"]),
-        markup_color="random",
-        single_word_mode=False,
-        repetitions=1,
-        p=0.33,
-    ),
-    Scribbles(
-        scribbles_type="lines",
-        scribbles_ink="random",
-        scribbles_location="random",
-        scribbles_size_range=(400, 600),
-        scribbles_count_range=(1, 6),
-        scribbles_thickness_range=(1, 3),
-        scribbles_brightness_change=[32, 64, 128],
-        scribbles_skeletonize=0,
-        scribbles_skeletonize_iterations=(2, 3),
-        scribbles_color="random",
-        scribbles_lines_stroke_count_range=(1, 2),
-        p=0.33,
-    ),
-    BadPhotoCopy(
-        noise_type=-1,
-        noise_side="random",
-        noise_iteration=(1, 2),
-        noise_size=(1, 3),
-        noise_value=(128, 196),
-        noise_sparsity=(0.3, 0.6),
-        noise_concentration=(0.1, 0.6),
-        blur_noise=random.choice([True, False]),
-        blur_noise_kernel=random.choice([(3, 3), (5, 5), (7, 7)]),
-        wave_pattern=random.choice([True, False]),
-        edge_effect=random.choice([True, False]),
-        p=0.33,
-    ),
-    Gamma(
-        gamma_range=(0.9, 1.1),
-        p=0.33,
-    ),
-    BindingsAndFasteners(
-        overlay_types="darken",
-        foreground=None,
-        effect_type="random",
-        ntimes=(2, 6),
-        nscales=(0.9, 1.0),
-        edge="random",
-        edge_offset=(10, 50),
-        use_figshare_library=0,
-        p=0.33,
-    ),
-    Faxify(
-        scale_range=(0.3, 0.6),
-        monochrome=random.choice([0, 1]),
-        monochrome_method="random",
-        monochrome_arguments={},
-        halftone=random.choice([0, 1]),
-        invert=1,
-        half_kernel_size=random.choice([(1, 1), (2, 2)]),
-        angle=(0, 360),
-        sigma=(1, 3),
-        p=0.33,
-    ),
-]
-
-pipeline = AugraphyPipeline(ink_phase=ink_phase, paper_phase=paper_phase, post_phase=post_phase)
+pipeline = AugraphyPipeline(ink_phase=[], paper_phase=[paperfactory], post_phase=[])
 
 for file in tqdm(os.listdir(os.path.join(PDF_DIR))):
     file_index = int(file.split('.')[0])
@@ -262,6 +38,11 @@ for file in tqdm(os.listdir(os.path.join(PDF_DIR))):
 
     for i in range(10):
         new_file_index = file_index * 10 + i
-        augmented_iamge = pipeline(image)
-        cv2.imwrite(os.path.join(INPUTS_DIR, str(new_file_index) + '.png'), image)
-        cv2.imwrite(os.path.join(OUTPUTS_DIR, str(new_file_index) + '.png'), augmented_iamge)
+        noise = np.random.normal(0, 30, image.shape).astype(np.uint8)
+        noised = cv2.add(image, noise)
+        blur_ksize = random.choice([5, 7, 9])
+        blurred = cv2.GaussianBlur(noised, (blur_ksize, blur_ksize), 0)
+        thresholded = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, random.randint(15, 20))
+        augmented_iamge = pipeline(thresholded)
+        cv2.imwrite(os.path.join(OUTPUTS_DIR, str(new_file_index) + '.png'), image)
+        cv2.imwrite(os.path.join(INPUTS_DIR, str(new_file_index) + '.png'), augmented_iamge)
